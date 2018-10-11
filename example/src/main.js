6f77f25dcaa9e962097064ea054dfc6b391a9574
my9074 <313231068@qq.com>
2018-10-11 15:01:22 +0800

    1.Add example dir 2.Update package.json lib

--- example/src/main.js
+++ example/src/main.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import App from "./App.vue";
-import SKElement from "./components/index";
+import SKElement from "@/components/index";
 
 Vue.config.productionTip = false;
 Vue.use(SKElement);
